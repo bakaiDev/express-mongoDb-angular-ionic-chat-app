06565656aee2db2f7035ba731f798f327d9a1d4d
BakayDev <gamempvs@gmail.com>
2020-02-27 02:12:49 +0600

    Auth Sign Up Service

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  signupUser() {
+    this.authService.registerUser('test').subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
