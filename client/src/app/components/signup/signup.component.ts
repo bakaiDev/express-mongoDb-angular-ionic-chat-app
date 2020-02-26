1a36efa4260eab9fe4b09512e776b2757589d42d
BakayDev <gamempvs@gmail.com>
2020-02-27 02:31:13 +0600

    Sign Up Reactive Form

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,13 +8,26 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.init();
+  }
+
+  init() {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    });
   }
 
   signupUser() {
+    console.log(this.signupForm.value);
     this.authService.registerUser('test').subscribe(data => {
       console.log(data);
     }, error => {
