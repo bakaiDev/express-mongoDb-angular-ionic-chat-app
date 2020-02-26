fe9b723c9773c7b7e3a68c1540b98f7b0ddc2997
BakayDev <gamempvs@gmail.com>
2020-02-26 21:58:37 +0600

    Added joi | Validate Sign Up Data

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,27 @@
+const Joi = require('joi');
+
 module.exports = {
     CreateUser(req, res) {
-        console.log(req.body)
+        const schema = Joi.object({
+            username: Joi.string()
+                .min(6)
+                .max(30)
+                .required(),
+            email: Joi.string()
+                .min(6)
+                .max(30)
+                .required()
+                .email(),
+            password: Joi.string()
+                .min(6)
+                .max(30)
+                .required() });
+
+        const {error, value} = schema.validate(req.body);
+
+        if (error && error.details) {
+            return res.status(500).json({message: error.details});
+        }
     }
 };
+
