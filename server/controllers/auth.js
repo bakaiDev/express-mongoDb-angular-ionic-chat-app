63cb8b1c1af286a0d742b7b41abd49870ef5c512
BakayDev <gamempvs@gmail.com>
2020-02-27 01:24:27 +0600

    Check If Email Exist

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,11 @@
 const Joi = require('joi');
 const HttpStatus = require('http-status-codes');
 
+const User = require('../models/userModels');
+const Helpers = require('../Hellpers');
+
 module.exports = {
-    CreateUser(req, res) {
+   async CreateUser(req, res) {
         const schema = Joi.object({
             username: Joi.string()
                 .min(6)
@@ -23,6 +26,11 @@ module.exports = {
         if (error && error.details) {
             return res.status(HttpStatus.BAD_REQUEST).json({message: error.details});
         }
+
+       const userEmail = await User.findOne({email: Helpers.lowerCase( req.body.email)});
+       if (userEmail) {
+           return res.status(HttpStatus.CONFLICT).json({message: 'Email already exist'});
+       }
     }
 };
 
