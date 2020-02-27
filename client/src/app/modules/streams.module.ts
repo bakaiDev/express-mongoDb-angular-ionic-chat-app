a6abd0040bfc5e192cb8183afdb229a70ecd502a
BakayDev <gamempvs@gmail.com>
2020-02-27 22:43:07 +0600

    Toolbar Component

--- client/src/app/modules/streams.module.ts
+++ client/src/app/modules/streams.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {StreamsComponent} from '../components/steams/streams.component';
 import {TokenService} from '../services/token.service';
+import {ToolbarComponent} from '../components/toolbar/toolbar.component';
 
 
 
 @NgModule({
-  declarations: [StreamsComponent],
+  declarations: [StreamsComponent, ToolbarComponent],
   imports: [
     CommonModule
   ],
+  exports: [StreamsComponent, ToolbarComponent],
   providers: [TokenService]
 })
 export class StreamsModule { }
