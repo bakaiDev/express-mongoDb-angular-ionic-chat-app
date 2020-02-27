0c733e0a0f770beb66f459bf7108b2868e04a254
BakayDev <gamempvs@gmail.com>
2020-02-27 18:06:04 +0600

    Added token Service Methods

--- client/src/app/services/token.service.ts
+++ client/src/app/services/token.service.ts
@@ -7,4 +7,16 @@ import {CookieService} from 'ngx-cookie-service';
 export class TokenService {
 
   constructor(private cookieService: CookieService) { }
+
+  setToken(token) {
+    this.cookieService.set('app_token', token);
+  }
+
+  getToken() {
+    return this.cookieService.get('app_token');
+  }
+
+  deleteToken() {
+     this.cookieService.delete('app_token');
+  }
 }
