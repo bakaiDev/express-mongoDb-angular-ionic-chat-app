ac26c1536d84d4ad8f7805c81f7f837fc6cd46c6
BakayDev <gamempvs@gmail.com>
2020-02-28 02:52:38 +0600

    Verify JWT Token in server middleware

---
+++ server/Hellpers/AuthHelper.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const HttpStatus = require('http-status-codes');
+const dbConfig = require('../config/secret');
+
+module.exports = {
+    VerifyToken : (req, res, next) => {
+        const token = req.cookie.auth;
+        if (token) {
+            return res.status(HttpStatus.FORBIDDEN).json({message: 'No token provided'})
+        }
+
+        return jwt.verify(token, dbConfig.secret, (err, decoded) => {
+            if (err) {
+                if (err.expiredAt < new Date()) {
+                    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                        message: 'Token Expired. Please login again',
+                        token: null
+                    });
+                }
+                next();
+            }
+            req.user = decoded.data;
+            next();
+        });
+    }
+};
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const PostController = require('../controllers/post');
+const AuthHelper = require('../Hellpers/AuthHelper');
 
-router.post('/post/add-post', PostController.AddPost);
+router.post('/post/add-post',AuthHelper.VerifyToken, PostController.AddPost);
 
 module.exports = router;
