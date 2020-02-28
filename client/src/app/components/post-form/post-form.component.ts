f01ccf6c5dcec6dcc24804ed1525ab43fb7d0139
BakayDev <gamempvs@gmail.com>
2020-02-28 15:35:49 +0600

    Install Socket.io Client

--- client/src/app/components/post-form/post-form.component.ts
+++ client/src/app/components/post-form/post-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PostService} from '../../services/post.service';
+import io from 'socket.io-client';
 
 @Component({
   selector: 'app-post-form',
@@ -9,10 +10,13 @@ import {PostService} from '../../services/post.service';
 })
 export class PostFormComponent implements OnInit {
   postForm: FormGroup;
+  socket: any;
 
   constructor(private fb: FormBuilder,
               private postService: PostService
-) { }
+) {
+    this.socket = io('http://localhost:3000');
+  }
 
   ngOnInit(): void {
     this.init();
