469f783a383df22336bbe0effa0a1b39ff839b04
BakayDev <gamempvs@gmail.com>
2020-02-28 23:41:16 +0600

    Get All Users Method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,15 @@
-module.exports = {
-    GetAllUsers(req, res) {
+const HttpStatus = require('http-status-codes');
 
+const User = require('../models/userModels');
+
+module.exports = {
+   async GetAllUsers(req, res) {
+       await User.find({})
+           .populate('posts.postsId')
+           .then((result) => {
+               res.status(HttpStatus.OK).json({message: 'All users', result});
+           }).catch(error => {
+               res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occurred'});
+           })
     }
 };
