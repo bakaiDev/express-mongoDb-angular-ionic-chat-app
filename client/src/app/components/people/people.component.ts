045bbf43838aa6c1e6e750c61431b571f08b9bbc
BakayDev <gamempvs@gmail.com>
2020-02-29 00:00:30 +0600

    Remove Logged in user

--- client/src/app/components/people/people.component.ts
+++ client/src/app/components/people/people.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersService} from '../../services/users.service';
+import _ from 'lodash';
+import {TokenService} from '../../services/token.service';
 
 @Component({
   selector: 'app-people',
@@ -8,15 +10,19 @@ import {UsersService} from '../../services/users.service';
 })
 export class PeopleComponent implements OnInit {
   users = [];
+  loggedInUser: any;
 
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService,
+              private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    this.loggedInUser = this.tokenService.getPayload();
     this.getUsers();
   }
 
   getUsers() {
     this.usersService.getAllUsers().subscribe((data) => {
+      _.remove(data.result, {username : this.loggedInUser});
       this.users = data.result;
     });
   }
